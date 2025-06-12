091ab1826281d9b1f954f61b9572112512722441
Joel Rosario <cirquitz@gmail.com>
2025-06-12 14:04:15 +0530

    Start ids with 1 instead of 0

--- src/main/java/com/store/model/Order.kt
+++ src/main/java/com/store/model/Order.kt
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger
 
 class Order(@field:Positive val productid: Int = 0, @field:Positive val count: Int = 0, @field:NotNull var status: OrderStatus = OrderStatus.pending, val id: Int = idGenerator.getAndIncrement()) {
     companion object {
-        val idGenerator: AtomicInteger = AtomicInteger()
+        val idGenerator: AtomicInteger = AtomicInteger(1)
     }
 }
 
--- src/main/java/com/store/model/Product.kt
+++ src/main/java/com/store/model/Product.kt
@@ -11,7 +11,7 @@ data class Product(
     val id: Int = idGenerator.getAndIncrement()
 ) {
     companion object {
-        val idGenerator: AtomicInteger = AtomicInteger()
+        val idGenerator: AtomicInteger = AtomicInteger(0)
     }
 }
 
