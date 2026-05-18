c5e471fa4228036fe11e11192ddb4f7863793071
Sushant092003 <bssushant2003@gmail.com>
2026-05-18 18:18:21 +0530

    fix: Add SpringBootTest annotation for ContractTest

--- src/test/java/com/store/ContractTest.java
+++ src/test/java/com/store/ContractTest.java
@@ -5,15 +5,21 @@ import io.specmatic.stub.ContractStub;
 import io.specmatic.test.SpecmaticContractTest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.condition.EnabledIf;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 
 import static io.specmatic.stub.API.createStub;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@EnabledIf(value = "isNonCIOrLinux", disabledReason = "Run only on Linux in CI; all platforms allowed locally")
 public class ContractTest implements SpecmaticContractTest {
-    private static ConfigurableApplicationContext context;
+    public static boolean isNonCIOrLinux() {
+        return !"true".equals(System.getenv("CI")) || System.getProperty("os.name").toLowerCase().contains("linux");
+    }
     private static ContractStub stub = null;
 
     @BeforeAll
@@ -23,8 +29,6 @@ public class ContractTest implements SpecmaticContractTest {
         DB.INSTANCE.resetDB();
 
         stub = createStub("localhost", 9000);
-
-        context = SpringApplication.run(Application.class);
     }
 
     @AfterAll
@@ -33,6 +37,5 @@ public class ContractTest implements SpecmaticContractTest {
             stub.close();
         }
 
-        context.close();
     }
 }
